48f90085160da8a000bffe7b19256e81f04343f9
GhostGuanyin <40558379+MonHer@users.noreply.github.com>
2020-04-01 00:39:54 +0800

    Class learning

---
+++ Object-oriented_OOP/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+#@Pjname ;PythonBasicLearning
+#@Time   :2020/03/31/23:24
+#@Author :Jiuyi
+#@File   :__init__.py.py
---
+++ Object-oriented_OOP/menu_oop.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @Pjname ;PythonBasicLearning
+# @Time   :2020/03/31/23:50
+# @Author :Jiuyi
+# @File   :menu_oop.py
+
+# 定义一个类
+class Menu(object):
+    Aioli = '蒜泥白菜'
+    Pickled_Konjac_Tofu = '酸菜魔芋豆腐'
+
+    def __init__(self, ty, yt):  # 构造函数
+        self.ty = ty
+        self.yt = yt
+
+    def eat(self):  # 定义方法
+        print("123456"+self.ty, self.yt)
+
+
+
+# 调用类
+I_want_to_eat = Menu("HJDHF", "FDJIH")
+I_want_to_eat2 = Menu("KK", "FDSB")
+print(I_want_to_eat.Aioli)
+print(I_want_to_eat.Pickled_Konjac_Tofu)
+I_want_to_eat.eat()
+I_want_to_eat2.eat()
