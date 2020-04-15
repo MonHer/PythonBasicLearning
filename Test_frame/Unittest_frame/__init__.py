a5b5e659151b185d2df7d229456e3baa37647546
GhostGuanyin <40558379+MonHer@users.noreply.github.com>
2020-04-16 01:44:20 +0800

    unittest learning

---
+++ Test_frame/Unittest_frame/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Pjname ; PythonBasicLearning
+# @Time   : 2020/04/15/23:53
+# @Author : Yuye
+# @File   : __init__.py.py
---
+++ Test_frame/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Pjname ; PythonBasicLearning
+# @Time   : 2020/04/15/23:51
+# @Author : Yuye
+# @File   : __init__.py.py
